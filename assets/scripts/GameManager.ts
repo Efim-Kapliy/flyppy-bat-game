92595c19d38c8ee67e0a867fc8a2bcbfd3df4d01
Efim <efim0424@mail.ru>
2025-09-03 16:04:06 +0300

    feat: add player and actions for him and prefabs for obstacles

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, input, Input, RigidBody2D } from 'cc';
+import { _decorator, Component, Node, input, Input, RigidBody2D, Vec2, Prefab, director } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameManager')
@@ -6,11 +6,21 @@ export class GameManager extends Component {
   @property(Node)
   player: Node;
 
+  @property(Prefab)
+  topObstacle: Prefab;
+
+  @property(Prefab)
+  bottomObstacle: Prefab;
+
   start() {
     input.on(Input.EventType.TOUCH_START, this.jump, this);
   }
 
-  private jump() {}
+  private jump() {
+    let body: RigidBody2D = this.player.getComponent(RigidBody2D);
+    body.linearVelocity = new Vec2(0, 0);
+    body.applyLinearImpulseToCenter(new Vec2(0, 700), true);
+  }
 
   update(deltaTime: number) {}
 }
